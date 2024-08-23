94313e3a385ccb16698701988a83e525c13f4f74
Ygor Furtado <ygorfurt@gmail.com>
2024-08-23 19:02:11 -0300

    Atualização

--- code/LerMatriz.py
+++ code/LerMatriz.py
@@ -2,34 +2,55 @@
 
 import networkx as nx
 import ygorfuncoes as yf
+import matplotlib.pyplot as plt
 
-grafo = yf.ler_csv("gephi_matriz1.csv")
+grafo = yf.ler_csv("gephi_matriz2.csv")
 
 def obter_caracteristicas(grafo):
     caracteristicas = {}
-    caracteristicas['densidade'] = nx.density(grafo)
     caracteristicas['nós'] = grafo.number_of_nodes()
     caracteristicas['arestas'] = grafo.number_of_edges()
-    caracteristicas['maior_componente'] = len(max(nx.connected_components(grafo), key=len))
-    caracteristicas['clustering_medio'] = nx.average_clustering(grafo)
+    caracteristicas['conectividade_de_nós'] = nx.node_connectivity(grafo)    
+    caracteristicas['conectividade_de_arestas'] = nx.edge_connectivity(grafo)
+    
+    caracteristicas['densidade'] = nx.density(grafo)
     caracteristicas['grau_medio'] = yf.grau_medio(grafo)
+    caracteristicas['clustering_medio'] = nx.average_clustering(grafo, weight='weight')
+    
+    caracteristicas['maior_componente'] = len(max(nx.connected_components(grafo), key=len))
     caracteristicas['diametros'] = yf.calcular_diametros(grafo)
+    
+    caracteristicas['centralidade_de_proximidade']= nx.closeness_centrality(grafo, distance='weight')
+    caracteristicas['centralidade_de_intermedialidade'] = nx.betweenness_centrality(grafo, weight='weight')
+    caracteristicas['clustering'] = nx.clustering(grafo,weight='weight')
+    
     
     return caracteristicas
 
+
 def exibir_resultados(caracteristicas):
     print(f"Número de nós: {caracteristicas['nós']}")
+    print(f"Conectividade de Nós: {caracteristicas['conectividade_de_nós']}")
     print(f"Número de arestas: {caracteristicas['arestas']}")
-    print(f"Densidade do grafo: {caracteristicas['densidade']:.4f}")
-    print(f"Grau medio do grafo: {caracteristicas['maior_componente']}")
-    print(f"Coeficiente médio de agrupamento: {caracteristicas['clustering_medio']:.4f}")
-    print(f"Tamanho do maior componente conexo: {caracteristicas['maior_componente']}")
+    print(f"Conectividade de Arestas: {caracteristicas['conectividade_de_arestas']}\n")
     
+    print(f"Densidade do grafo: {caracteristicas['densidade']:.4f}")
+    print(f"Grau medio do grafo: {caracteristicas['grau_medio']:.4f}")
+    print(f"Coeficiente médio de agrupamento: {caracteristicas['clustering_medio']:.4f}\n")
+    
+    
+    print(f"Tamanho do maior componente conexo: {caracteristicas['maior_componente']}")
     if len(caracteristicas['diametros']) == 1:
-        print(f"Diâmetro do grafo: {caracteristicas['diametros'][0]}")
+        print(f"Diâmetro do grafo: {caracteristicas['diametros'][0]}\n")
     else:
         for i, diametro in enumerate(caracteristicas['diametros']):
             print(f"Diâmetro do componente conexo {i + 1}: {diametro}")
+        print("\n")
+    
+    print(f"Clustering: {caracteristicas['clustering']}\n")
+    print(f"centralidade_de_proximidade: {caracteristicas['centralidade_de_proximidade']}\n")
+    print(f"centralidade_de_intermedialidade: {caracteristicas['centralidade_de_intermedialidade']}\n")
 
 caracteristicas = obter_caracteristicas(grafo)
 exibir_resultados(caracteristicas)
+yf.salvar_resultados(caracteristicas,nome_arquivo="Resultados.txt")
--- code/ygorfuncoes.py
+++ code/ygorfuncoes.py
@@ -15,9 +15,8 @@ def ler_csv(caminho):
     return grafo
 
 def grau_medio(grafo):
-    graus = [grau for _, grau in grafo.degree()]
-    grau_medio = np.mean(graus)
-    return grau_medio
+    graumean = sum(dict(grafo.degree(weight="weight")).values())
+    return graumean / grafo.number_of_nodes()
 
 def calcular_diametros(grafo):
     componentes = list(nx.connected_components(grafo))
@@ -25,8 +24,39 @@ def calcular_diametros(grafo):
     
     for componente in componentes:
         subgrafo = grafo.subgraph(componente)
-        diametros.append(nx.diameter(subgrafo))
+        diametros.append(nx.diameter(subgrafo,weight="weight"))
         
     diametros.sort(reverse=True)
     
     return diametros
+
+def salvar_resultados(caracteristicas, nome_arquivo):
+    with open(nome_arquivo, 'w', encoding='utf-8') as arquivo:
+        arquivo.write(f"Número de nós: {caracteristicas['nós']}\n")
+        arquivo.write(f"Conectividade de Nós: {caracteristicas['conectividade_de_nós']}\n")
+        arquivo.write(f"Número de arestas: {caracteristicas['arestas']}\n")
+        arquivo.write(f"Conectividade de Arestas: {caracteristicas['conectividade_de_arestas']}\n\n")
+        
+        arquivo.write(f"Densidade do grafo: {caracteristicas['densidade']:.4f}\n")
+        arquivo.write(f"Grau médio do grafo: {caracteristicas['grau_medio']:.4f}\n")
+        arquivo.write(f"Coeficiente médio de agrupamento: {caracteristicas['clustering_medio']:.4f}\n\n")
+        
+        arquivo.write(f"Tamanho do maior componente conexo: {caracteristicas['maior_componente']}\n")
+        if len(caracteristicas['diametros']) == 1:
+            arquivo.write(f"Diâmetro do grafo: {caracteristicas['diametros'][0]}\n\n")
+        else:
+            for i, diametro in enumerate(caracteristicas['diametros']):
+                arquivo.write(f"Diâmetro do componente conexo {i + 1}: {diametro}\n")
+            arquivo.write("\n")
+        
+        arquivo.write("\nCoeficiente de Agrupamento por Nó:\n")
+        for node, clustering in caracteristicas['clustering'].items():
+            arquivo.write(f"  Nó {node}: {clustering:.4f}\n")
+
+        arquivo.write(f"Centralidade de Proximidade:\n")
+        for node, centralidade in caracteristicas['centralidade_de_proximidade'].items():
+            arquivo.write(f"  Nó {node}: {centralidade:.4f}\n")
+        
+        arquivo.write(f"\nCentralidade de Intermediabilidade:\n")
+        for node, centralidade in caracteristicas['centralidade_de_intermedialidade'].items():
+            arquivo.write(f"  Nó {node}: {centralidade:.4f}\n")
